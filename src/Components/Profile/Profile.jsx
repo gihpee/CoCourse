44d9581f57c9139929f90992fffaa423bec44c84
gihpee <gihpeee@gmail.com>
2023-12-09 16:24:11 +0300

    test id

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -2,10 +2,9 @@ import React from "react";
 import "./Profile.css";
 
 function Home() {
-  const { first_name, photo_url } = window.Telegram.WebApp.initDataUnsafe.user;
+  const { id } = window.Telegram.WebApp.initDataUnsafe.user;
 
-  return <div>{first_name}
-        <img src={photo_url} alt='' /></div>;
+  return <div>{id}</div>;
 }
 
 export default Home;
