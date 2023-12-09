bfef367a143b117cdef47aa180bf6e92330325e5
gihpee <gihpeee@gmail.com>
2023-12-09 16:34:52 +0300

    test profile prev

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import "./Profile.css";
 
 function Home() {
-  const { id } = window.Telegram.WebApp.initDataUnsafe.user;
+  const { first_name, last_name, photo_url } = window.Telegram.WebApp.initDataUnsafe.user;
 
-  return <div>{id}</div>;
+  return <>
+          <div className="prev" style={{backgroundImage: `url(${photo_url})`}}>
+            <p>{ first_name + ' ' + last_name }</p>
+          </div>
+        </>;
 }
 
 export default Home;
