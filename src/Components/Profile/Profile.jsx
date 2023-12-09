c29e4cf242a66d1adf82be818ee90a11afe5956a
gihpee <gihpeee@gmail.com>
2023-12-09 16:14:18 +0300

    test

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Profile.css";
 
 function Home() {
-  const { first_name, last_name, username } = window.Telegram.WebApp.initDataUnsafe.user;
+  const { first_name } = window.Telegram.WebApp.initDataUnsafe.user;
 
   return <div>{first_name}</div>;
 }
