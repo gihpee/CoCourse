72dcaa5b8fcbdb6df963fb2cd1823440a828209e
gihpee <gihpeee@gmail.com>
2024-01-20 22:45:37 +0300

    fix error

--- src/Components/Feedback/SendFeedback.jsx
+++ src/Components/Feedback/SendFeedback.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useEffect } from 'react';
-import { json, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import "./SendFeedback.css";
 
