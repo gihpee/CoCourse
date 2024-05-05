97afc503df793962c5cb6d48ff192af95a480ad9
gihpee <gihpeee@gmail.com>
2024-05-05 20:01:48 +0300

    test feedback

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -144,7 +144,7 @@ function Home() {
               </Link>*/}
           <div className="edit_container">
             <span>Отзывы</span>
-            <Link to={`/user-feedback/${userData.id}`} className="feedback">
+            <Link to={`/user-feedback/${userData.id}`} className="feedback" style={{width: '100%'}}>
                       <div className="rate">{20 * averageRate > 50 ? <p>{averageRate}</p> : <p style={{color: 'white'}}>{averageRate}</p>}</div>
                       <div className="row_grad_l">
                           <div className="grad_l" style={{width: `calc((100% / 5) * ${averageRate})`, background: `linear-gradient(to right, #EA4A4F 0%, #D8BB55, #7EBB69 calc(500% / ${averageRate}))`}}></div>
