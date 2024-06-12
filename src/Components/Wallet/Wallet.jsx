fe31218e83f1571077908ad87917b0b64db546dc
gihpee <gihpeee@gmail.com>
2024-06-13 02:35:58 +0300

    test wallet

--- src/Components/Wallet/Wallet.jsx
+++ src/Components/Wallet/Wallet.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-import { TonConnectButton } from '@tonconnect/ui-react';
+import { TonConnectButton, useTonAddress, useTonWallet } from '@tonconnect/ui-react';
 
 
 function Wallet() {
+    const userFriendlyAddress = useTonAddress();
+    const rawAddress = useTonAddress(false);
+    const wallet = useTonWallet();
+    
+
     return (
         <div className="column" style={{minHeight: '100vh'}}>
             <TonConnectButton />
+            <span>User-friendly address: {userFriendlyAddress}</span>
+            <span>Connected wallet: {wallet.name}</span>
+            <span>Device: {wallet.device.appName}</span>
         </div>
     );
 }
