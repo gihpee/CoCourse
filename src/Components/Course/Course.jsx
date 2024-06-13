13623ae4e89ca6d7dbb684348321b8eea794c013
gihpee <gihpeee@gmail.com>
2024-06-13 14:56:37 +0300

    fix bug

--- src/Components/Course/Course.jsx
+++ src/Components/Course/Course.jsx
@@ -12,8 +12,8 @@ import "./Course.css";
 
 function Course() {
     window.scrollTo(0, 0)
-    const { id } = useParams();
-    const { user_id } = window.Telegram.WebApp.initDataUnsafe.user;
+    const { course_id } = useParams();
+    const { id } = window.Telegram.WebApp.initDataUnsafe.user;
 
     const [data, setData] = useState([]);
     const [userData, setUserData] = useState([]);
@@ -29,16 +29,16 @@ function Course() {
     useEffect(() => {
         const fetchData = async () => {
         try {
-            const response = await fetch(`https://commoncourse.io/getcourse?id=${id}`)
+            const response = await fetch(`https://commoncourse.io/getcourse?id=${course_id}`)
             const result = await response.json();
 
             const response_user = await fetch(`https://commoncourse.io/user?id=${result[0].user}`)
             const result_user = await response_user.json();
 
-            const response_paid = await fetch(`https://commoncourse.io/user-paid-courses?id=${user_id}`);
+            const response_paid = await fetch(`https://commoncourse.io/user-paid-courses?id=${id}`);
             const result_paid = await response_paid.json();
 
-            const response_own = await fetch(`https://commoncourse.io/user-made-courses?id=${user_id}`);
+            const response_own = await fetch(`https://commoncourse.io/user-made-courses?id=${id}`);
             const result_own = await response_own.json();
             
             setUserCourses(result_paid);
@@ -53,14 +53,14 @@ function Course() {
         };
 
         fetchData();
-    }, [id, user_id]);
+    }, [course_id, id]);
 
     if (data.length === 0) {
         return <div className="loading"></div>; // или что-то другое, пока данные загружаются
     }
 
-    const paid = userCourses.some(course => course.course_id === id);
-    const own = coursesData.some(course => course.id === id);
+    const paid = userCourses.some(course => course.course_id === course_id);
+    const own = coursesData.some(course => course.id === course_id);
 
     const myTransaction = {
         validUntil: Math.floor(Date.now() / 1000) + 60, // 60 sec
@@ -98,7 +98,7 @@ function Course() {
     }
 
 
-    if (id === '79') {
+    if (course_id === '79') {
         userData[0].username = 'HowToCommonCourse';
     }
 
@@ -124,13 +124,13 @@ function Course() {
             </div>
             <span>Отзывы</span>
             <div className="feedback">
-                <Link to={`/course-feedback/${id}`}>
+                <Link to={`/course-feedback/${course_id}`}>
                 <div className="rate">{20 * averageRate > 50 ? <p>{averageRate}</p> : <p style={{color: 'white'}}>{averageRate}</p>}</div>
                 <div className="row_grad_l">
                     <div className="grad_l" style={{width: `calc((100% / 5) * ${averageRate})`, background: `linear-gradient(to right, #EA4A4F 0%, #D8BB55, #7EBB69 calc(500% / ${averageRate}))`}}></div>
                 </div>
                 </Link>
-                <Link to={`/send-feedback/${id}`}>
+                <Link to={`/send-feedback/${course_id}`}>
                     <div className="billet">
                         <img src={star} alt='' />
                         <p>Оставить отзыв</p>
