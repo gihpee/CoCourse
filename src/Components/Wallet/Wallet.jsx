d6a11c1dafcfab5e752f63cff687e75a9244fa28
gihpee <gihpeee@gmail.com>
2024-06-16 00:04:50 +0300

    test

--- src/Components/Wallet/Wallet.jsx
+++ src/Components/Wallet/Wallet.jsx
@@ -1,23 +1,8 @@
 import React from "react";
-import { TonConnectButton, useTonAddress, useTonConnectUI } from '@tonconnect/ui-react';
+import { TonConnectButton } from '@tonconnect/ui-react';
 
 
 function Wallet() {
-    const userFriendlyAddress = useTonAddress();
-    const [tonConnectUI, setOptions] = useTonConnectUI();
-    setOptions({ language: 'ru' });
-
-    const myTransaction = {
-        validUntil: Math.floor(Date.now() / 1000) + 60, // 60 sec
-        messages: [
-            {
-                address: "EQBBJBB3HagsujBqVfqeDUPJ0kXjgTPLWPFFffuNXNiJL0aA",
-                amount: "20000000",
-                // stateInit: "base64bocblahblahblah==" // just for instance. Replace with your transaction initState or remove
-            }
-        ]
-    }
-    
 
     return (
         <div className="column" style={{minHeight: '100vh'}}>
