bef60c0c37fe53a9506b44f2d9aa20f51f1bb8ec
gihpee <gihpeee@gmail.com>
2024-06-30 05:54:00 +0300

    truying to fix bug

--- vite.config.js
+++ vite.config.js
@@ -1,43 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
-import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
+import { defineConfig } from 'vite'
+import nodePolyfills from 'vite-plugin-node-stdlib-browser'
 
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
-  optimizeDeps: {
-    esbuildOptions: {
-      // Node.js global to browser globalThis
-      define: {
-        global: 'globalThis'
-      },
-      // Enable esbuild polyfill plugins
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true
-        }),
-        NodeModulesPolyfillPlugin()
-      ]
-    }
-  },
-  resolve: {
-    alias: {
-      // This Rollup alias is required to ensure that any
-      // import to the 'buffer' package will resolve to the
-      // version installed in your project.
-      buffer: 'buffer'
-    }
-  },
-  build: {
-    rollupOptions: {
-      plugins: [
-        // Enable rollup polyfills plugin
-        // used during production bundling
-        rollupNodePolyFill()
-      ]
-    }
-  }
-});
+  // other options
+  plugins: [nodePolyfills()]
+})
