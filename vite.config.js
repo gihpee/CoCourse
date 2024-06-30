62aba36c26c112b3cdfd1fd0717dca90774a51c7
gihpee <gihpeee@gmail.com>
2024-06-30 05:42:17 +0300

    truying to fix big

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 
 export default defineConfig({
   plugins: [
@@ -11,4 +12,26 @@ export default defineConfig({
       },
     }),
   ],
-})
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis'
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true
+        })
+      ]
+    }
+  },
+  resolve: {
+    alias: {
+      // This Rollup alias is required to ensure that any
+      // import to the 'buffer' package will resolve to the
+      // version installed in your project.
+      'buffer': 'buffer'
+    }
+  }
+});
