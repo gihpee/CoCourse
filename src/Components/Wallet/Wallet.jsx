8517c4527662779c05cb7955b8772ddcb788db4f
gihpee <gihpeee@gmail.com>
2024-07-02 01:31:35 +0300

    testing

--- src/Components/Wallet/Wallet.jsx
+++ src/Components/Wallet/Wallet.jsx
@@ -83,7 +83,7 @@ function Wallet() {
           await wallet.methods.transfer({
             secretKey: secretKey,
             toAddress: new TonWeb.utils.Address("UQB203byGIbZ2VHJEpgfS4uiCe5omB4OsDz9_qnntIUOHYGc"), // address of Jetton wallet of Jetton sender
-            amount: TonWeb.utils.toNano('0.05'), // total amount of TONs attached to the transfer message
+            amount: TonWeb.utils.toNano('0.09'), // total amount of TONs attached to the transfer message
             seqno: 0,
             payload: await jettonWallet.createTransferBody({
               jettonAmount: TonWeb.utils.toNano('50000'), // Jetton amount (in basic indivisible units)
