02afcc61a1583c874fe65ffa3fb24fd297253866
gihpee <gihpeee@gmail.com>
2024-07-02 09:48:25 +0300

    fix bug

--- src/Components/Course/Course.jsx
+++ src/Components/Course/Course.jsx
@@ -11,7 +11,7 @@ import { useTonAddress } from '@tonconnect/ui-react';
 //import { beginCell, toNano, Address } from '@ton/ton'
 import TonWeb from "tonweb";
 import { mnemonicToSeed } from 'tonweb-mnemonic';
-import { useTelegram } from '@twa-dev/sdk/react';
+import { useTelegram } from '@twa-dev/sdk';
 import "./Course.css";
 
 
