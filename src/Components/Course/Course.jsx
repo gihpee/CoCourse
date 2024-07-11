09df77e70173cae68181d84f49a1a314cb1b8dca
gihpee <gihpeee@gmail.com>
2024-07-11 21:30:29 +0300

    fix bug

--- src/Components/Course/Course.jsx
+++ src/Components/Course/Course.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import calend from '../assets/course/calender.svg'
-import star from '../assets/course/star.svg'
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
