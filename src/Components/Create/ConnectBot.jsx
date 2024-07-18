7bcdf1bfb618aa9789f41e48d6a0f571d9e8426b
gihpee <gihpeee@gmail.com>
2024-07-18 15:51:40 +0300

    check

--- src/Components/Create/ConnectBot.jsx
+++ src/Components/Create/ConnectBot.jsx
@@ -10,7 +10,7 @@ function ConnectBot() {
     const handleButtonClick = () => {
         const botUsername = "CoCourseBot";
         const link = `https://t.me/${botUsername}?startchannel=1`;
-        tg.WebApp.open(link)
+        tg.WebApp.openLink(link);
     };
 
     return <> 
