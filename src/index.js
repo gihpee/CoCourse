80166cb1518bab748156199522bee9fcadac58d6
gihpee <gihpeee@gmail.com>
2024-12-04 01:49:00 +0300

    add eruda

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import {BrowserRouter} from "react-router-dom";
 import "./index.css";
+import eruda from 'eruda';
 import App from "./App";
 import './fonts/NeueMachina/NeueMachina-Regular.ttf';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+eruda.init()
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
