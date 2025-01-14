b22daf82b04133ae1395bcc4660d0b858fa60d77
KoUD2 <kostik-udod@mail.ru>
2025-01-14 15:35:52 +0300

    remove extra files

--- src/Components/Create/Create.jsx
+++
@@ -1,7 +0,0 @@
-import "./Create.css";
-
-function Create() {
-  return <div>Содержимое раздела создать</div>;
-}
-
-export default Create;
--- src/Components/Profile/Profile.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Profile.css";
-
-function Home() {
-  return <div>Содержимое раздела профиль</div>;
-}
-
-export default Home;
