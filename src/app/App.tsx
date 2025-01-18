d07d997cab42be5473092e0c77d9adb55842f606
KoUD2 <kostik-udod@mail.ru>
2025-01-18 14:36:11 +0300

    create links

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import { TonConnectUIProvider } from '@tonconnect/ui-react'
 import { useEffect } from 'react'
-import { Route, Routes, useNavigate, useParams } from 'react-router-dom'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import BuyCourse from '../pages/Course/BuyCourse'
 import Course from '../pages/Course/Course'
 import ConnectBot from '../pages/Create/ConnectBot'
@@ -38,11 +38,12 @@ function App() {
 	tg.WebApp.enableClosingConfirmation()
 	const navigate = useNavigate()
 
-	const { startParam } = useParams()
-	console.log('1 startParam', startParam)
-
 	useEffect(() => {
-		console.log('2 startParam', startParam)
+		const urlParams = new URLSearchParams(window.location.search)
+		console.log('urlParams', urlParams)
+		const startParam = urlParams.get('start_param')
+		console.log('startParam', startParam)
+
 		if (startParam) {
 			if (startParam === 'profile') {
 				navigate('/profile')
@@ -51,7 +52,7 @@ function App() {
 				navigate(`/course/${courseId}`)
 			}
 		}
-	}, [navigate, startParam])
+	}, [navigate])
 
 	return (
 		<TonConnectUIProvider manifestUrl='https://cosmic-axolotl-6ea6bd.netlify.app/tonconnect-manifest.json'>
