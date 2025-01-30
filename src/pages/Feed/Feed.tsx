23b803f9ea4a15e759193cac9813b55487fb8960
KoUD2 <kostik-udod@mail.ru>
2025-01-30 11:10:52 +0300

    remove no needed values in feed component

--- src/pages/Feed/Feed.tsx
+++ src/pages/Feed/Feed.tsx
@@ -73,7 +73,7 @@ function Feed() {
 				}
 			>
 				{!isPending ? (
-					<CardList courses={filteredData} userCourses={userCourses} />
+					<CardList courses={filteredDataWithMain} userCourses={userCourses} />
 				) : (
 					<div>Загрузка...</div>
 				)}
