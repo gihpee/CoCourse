516d03e3452bdf7b22221c18ef0959a2f16cb2b5
KoUD2 <kostik-udod@mail.ru>
2025-02-17 15:37:45 +0300

    update padding top to all pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -54,13 +54,6 @@ function App() {
 
 			postEvent('web_app_request_fullscreen')
 
-			const overflow = 100
-			document.body.style.overflowY = 'hidden'
-			document.body.style.marginTop = `${overflow}px`
-			document.body.style.height = window.innerHeight + overflow + 'px'
-			document.body.style.paddingBottom = `${overflow}px`
-			window.scrollTo(0, overflow)
-
 			console.log('swipeBehavior.isSupported()', swipeBehavior.isSupported())
 
 			if (swipeBehavior.isSupported()) {
