8aa9040f84790b4593d8d971c9a06dcf4f7f5910
KoUD2 <kostik-udod@mail.ru>
2025-02-20 10:22:13 +0300

    update course page

--- src/features/courses/components/CourseCard/CourseCard.tsx
+++ src/features/courses/components/CourseCard/CourseCard.tsx
@@ -22,94 +22,98 @@ const CourseCard: FC<ICourseCard> = ({
 	averageRate,
 	isCoursePage,
 }) => {
-	return (
-		<Link to={`/course/${itemCard.id}`} key={itemCard.id}>
-			<div className={styles['course-card']}>
-				{isCoursePage ? (
-					<div className={styles['course-card__header']}>
-						<div className={styles['course-card__person']}>
-							<div
-								className={styles['course-card__person-avatar']}
-								style={{
-									backgroundImage: `url(https://comncoursetest.ru${itemCard.user.photo_url})`,
-								}}
-							></div>
-							<h2 className={styles['course-card__person-name']}>
-								{itemCard.user.first_name + ' ' + itemCard.user.last_name}
-							</h2>
-						</div>
-						<div className={styles['course-card__buttons']}>
-							<CourseButton alt='Добавить в избранное' imgSrc={HeartIcon} />
-							<CourseButton alt='Поделиться' imgSrc={ShareIcon} />
-						</div>
+	const content = (
+		<div className={styles['course-card']}>
+			{isCoursePage ? (
+				<div className={styles['course-card__header']}>
+					<div className={styles['course-card__person']}>
+						<div
+							className={styles['course-card__person-avatar']}
+							style={{
+								backgroundImage: `url(https://comncoursetest.ru${itemCard.user.photo_url})`,
+							}}
+						></div>
+						<h2 className={styles['course-card__person-name']}>
+							{itemCard.user.first_name + ' ' + itemCard.user.last_name}
+						</h2>
+					</div>
+					<div className={styles['course-card__buttons']}>
+						<CourseButton alt='Добавить в избранное' imgSrc={HeartIcon} />
+						<CourseButton alt='Поделиться' imgSrc={ShareIcon} />
+					</div>
+				</div>
+			) : null}
+			<div className={styles['course-card__image-wrapper']}>
+				{itemCard.is_draft ? (
+					<div className={styles['course-card__status']}>
+						<div
+							className={cn(
+								styles['course-card__status-icon'],
+								styles['course-card__status-icon_status-draft']
+							)}
+						/>
+						<p className={styles['course-card__status-text']}>Черновик</p>
+					</div>
+				) : itemCard.on_moderation ? (
+					<div className={styles['course-card__status']}>
+						<div
+							className={cn(
+								styles['course-card__status-icon'],
+								styles['course-card__status-icon_status-moderation']
+							)}
+						/>
+						<p className={styles['course-card__status-text']}>На модерации</p>
 					</div>
 				) : null}
-				<div className={styles['course-card__image-wrapper']}>
-					{itemCard.is_draft ? (
-						<div className={styles['course-card__status']}>
-							<div
-								className={cn(
-									styles['course-card__status-icon'],
-									styles['course-card__status-icon_status-draft']
-								)}
-							/>
-							<p className={styles['course-card__status-text']}>Черновик</p>
-						</div>
-					) : itemCard.on_moderation ? (
-						<div className={styles['course-card__status']}>
-							<div
-								className={cn(
-									styles['course-card__status-icon'],
-									styles['course-card__status-icon_status-maderation']
-								)}
-							/>
-							<p className={styles['course-card__status-text']}>На модерации</p>
-						</div>
-					) : null}
-
-					{!isCoursePage ? (
-						<div className={styles['course-card__buttons']}>
-							<CourseButton alt='Добавить в избранное' imgSrc={HeartIcon} />
-							<CourseButton alt='Поделиться' imgSrc={ShareIcon} />
-						</div>
-					) : null}
-
-					<div
-						className={styles['course-card__image']}
-						style={{
-							backgroundImage: `url(${setImagePath(
-								chanelPhoto,
-								emptyHorizontalImage
-							)})`,
-						}}
-					/>
-				</div>
 
 				{!isCoursePage ? (
-					<CourseRating
-						amountOfStudents={amountOfStudents || 0}
-						averageRate={averageRate || 0}
-					/>
+					<div className={styles['course-card__buttons']}>
+						<CourseButton alt='Добавить в избранное' imgSrc={HeartIcon} />
+						<CourseButton alt='Поделиться' imgSrc={ShareIcon} />
+					</div>
 				) : null}
 
-				<CourseInfo
-					title={chanelName || 'Название курса'}
-					university={university || 'Неизвестный университет'}
+				<div
+					className={styles['course-card__image']}
+					style={{
+						backgroundImage: `url(${setImagePath(
+							chanelPhoto,
+							emptyHorizontalImage
+						)})`,
+					}}
 				/>
-
-				<div className={styles['course-card__footer']}>
-					<div className={styles['course-card__telegram-stars']}>
-						<img
-							src={TelegramPremiumStarIcon}
-							alt='Телеграм звёзды'
-							className={styles['course-card__stars-img']}
-						/>
-						<p className={styles['course-card__stars-count']}>12</p>
-					</div>
-					<h1 className={styles['course-card__price']}>{price} ₽</h1>
-				</div>
 			</div>
-		</Link>
+
+			{!isCoursePage ? (
+				<CourseRating
+					amountOfStudents={amountOfStudents || 0}
+					averageRate={averageRate || 0}
+				/>
+			) : null}
+
+			<CourseInfo
+				title={chanelName || 'Название курса'}
+				university={university || 'Неизвестный университет'}
+			/>
+
+			<div className={styles['course-card__footer']}>
+				<div className={styles['course-card__telegram-stars']}>
+					<img
+						src={TelegramPremiumStarIcon}
+						alt='Телеграм звёзды'
+						className={styles['course-card__stars-img']}
+					/>
+					<p className={styles['course-card__stars-count']}>12</p>
+				</div>
+				<h1 className={styles['course-card__price']}>{price} ₽</h1>
+			</div>
+		</div>
+	)
+
+	return isCoursePage ? (
+		content
+	) : (
+		<Link to={`/course/${itemCard.id}`}>{content}</Link>
 	)
 }
 
